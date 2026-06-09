593dd70386d56f0f9ea89da67f7d460acba64658
Ikwuka Okoye <ikwukao@proton.me>
2026-06-09 17:50:10 +0100

    Complete and call FixedAmountDiscount class

--- main.py
+++ main.py
@@ -29,9 +29,20 @@ class PercentageDiscount(DiscountStrategy):
             return True
         else:
             return False
-        
+
     def apply_discount(self, product: Product) -> float:
         return product.price * (1 - self.percent / 100)
+
+
+class FixedAmountDiscount(DiscountStrategy):
+    def __init__(self, amount: int) -> None:
+        self.amount = amount
+
+    def is_applicable(self, product: Product, user_tier: str) -> bool:
+        return product.price * 0.9 > self.amount
+
+    def apply_discount(self, product: Product) -> float:
+        return product.price - self.amount
 
 
 product = Product("Wireless Mouse", 50.0)
@@ -40,3 +51,5 @@ print(product)  # Wireless Mouse - $50.0
 discount = PercentageDiscount(10)
 print(discount.apply_discount(product))
 
+fixed_discount = FixedAmountDiscount(5)
+print(fixed_discount.apply_discount(product))
